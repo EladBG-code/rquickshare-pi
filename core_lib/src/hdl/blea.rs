03bbfbadaa99569a41a6d1dce2bed7b04c7fe21a
EladBG-code <EladBG-code@users.noreply.github.com>
2026-05-14 22:54:06 +0300

    Keep Quick Share BLE advert non-connectable

--- core_lib/src/hdl/blea.rs
+++ core_lib/src/hdl/blea.rs
@@ -43,9 +43,6 @@ impl BleAdvertiser {
         );
 
         let service_uuid = Uuid::from_u16(0xFE2C);
-        let initial_discoverable = self.adapter.is_discoverable().await.unwrap_or(false);
-        let initial_discoverable_timeout = self.adapter.discoverable_timeout().await.unwrap_or(180);
-        self.set_visible_mode(true).await;
         let handle = match self
             .advertise(service_uuid, SERVICE_DATA, Type::Broadcast)
             .await
@@ -63,8 +60,6 @@ impl BleAdvertiser {
         ctk.cancelled().await;
         info!("{INNER_NAME}: tracker cancelled, returning");
         drop(handle);
-        self.restore_visible_mode(initial_discoverable, initial_discoverable_timeout)
-            .await;
 
         Ok(())
     }
@@ -81,12 +76,9 @@ impl BleAdvertiser {
         );
 
         let service_uuid = Uuid::from_u16(0xFE2C);
-        let initial_discoverable = self.adapter.is_discoverable().await.unwrap_or(false);
-        let initial_discoverable_timeout = self.adapter.discoverable_timeout().await.unwrap_or(180);
         let mut handle = if *visibility_receiver.borrow() == Visibility::Invisible {
             None
         } else {
-            self.set_visible_mode(true).await;
             Some(self.start_advertising(service_uuid).await?)
         };
 
@@ -95,7 +87,6 @@ impl BleAdvertiser {
                 _ = ctk.cancelled() => {
                     info!("{INNER_NAME}: tracker cancelled, returning");
                     drop(handle);
-                    self.restore_visible_mode(initial_discoverable, initial_discoverable_timeout).await;
                     return Ok(());
                 }
                 changed = visibility_receiver.changed() => {
@@ -105,9 +96,7 @@ impl BleAdvertiser {
 
                     if visibility == Visibility::Invisible {
                         handle = None;
-                        self.set_visible_mode(false).await;
                     } else if handle.is_none() {
-                        self.set_visible_mode(true).await;
                         handle = Some(self.start_advertising(service_uuid).await?);
                     }
                 }
@@ -157,39 +146,11 @@ impl BleAdvertiser {
             advertisement_type,
             service_data: [(service_uuid, adv_data.into())].into(),
             discoverable: is_peripheral.then_some(true),
-            discoverable_timeout: is_peripheral.then_some(Duration::from_secs(0)),
+            discoverable_timeout: None,
             local_name: is_peripheral.then(default_device_name),
             min_interval: Some(Duration::from_millis(100)),
             max_interval: Some(Duration::from_millis(100)),
             ..Default::default()
-        }
-    }
-
-    async fn set_visible_mode(&self, discoverable: bool) {
-        if discoverable {
-            if let Err(err) = self.adapter.set_discoverable_timeout(0).await {
-                warn!("{INNER_NAME}: could not disable adapter discoverable timeout: {err}");
-            }
-        }
-
-        if let Err(err) = self.adapter.set_discoverable(discoverable).await {
-            warn!("{INNER_NAME}: could not set adapter discoverable={discoverable}: {err}");
-        }
-    }
-
-    async fn restore_visible_mode(&self, discoverable: bool, discoverable_timeout: u32) {
-        if let Err(err) = self.adapter.set_discoverable(discoverable).await {
-            warn!("{INNER_NAME}: could not restore adapter discoverable={discoverable}: {err}");
-        }
-
-        if let Err(err) = self
-            .adapter
-            .set_discoverable_timeout(discoverable_timeout)
-            .await
-        {
-            warn!(
-                "{INNER_NAME}: could not restore adapter discoverable timeout={discoverable_timeout}: {err}"
-            );
         }
     }
 }
